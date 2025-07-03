fd1d38e6c75f82d686a03bd03af0ae4f1e35a316
ARCHITA DEY <deyarchita26@gmail.com>
2025-07-03 19:34:55 +0530

    commit!!

--- dsa_sheet/src/Array/largestEle.java
+++ dsa_sheet/src/Array/largestEle.java
@@ -1,4 +1,34 @@
 package Array;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class largestEle {
+    public static void main(String[] args) {
+        int arr[]= new int[4];
+        Scanner sc= new Scanner(System.in);
+        for(int i=0; i<arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i]>arr[i+1]){
+//                int high= Math.max(arr[i], arr[i+1]);
+//                max=high;
+//            }
+//        }
+        Arrays.sort(arr);
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        HashSet<Integer>set= new HashSet<>();
+        for(int i=0; i< arr.length; i++){
+            set.add(arr[i]);
+        }
+        System.out.println(set);
+        Integer arr1[]= set.toArray(new Integer[set.size()]);
+        System.out.println("largest:"+ arr1[arr1.length-1]);
+        System.out.println("seclargest:"+ arr1[arr1.length-2]);
+    }
 }
---
+++ dsa_sheet/src/Array/rotateArray.java
@@ -0,0 +1,9 @@
+package Array;
+import java.util.*;
+public class rotateArray {
+    public static void rotate(int arr[], int k){
+        for(int i=0; i<arr.length; i++){
+            arr[k]
+        }
+    }
+}
